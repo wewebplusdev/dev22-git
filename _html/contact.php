80deaf8479c964ff97426df024abfb9a9fd49b81
Ohm <jeraphan2000@gmail.com>
2022-10-05 18:11:26 +0700

    สมาชิกสัมพันธ์, ติดต่อเรา 80%

---
+++ _html/contact.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('inc/metatag.php'); ?>
+    <?php include('inc/loadstyle.php'); ?>
+</head>
+
+<body>
+
+    <div class="global-container">
+        <?php include('inc/header.php'); ?>
+
+        <section class="site-container">
+
+            <div class="default-header">
+                <div class="top-graphic mb-4 text-primary">
+                    <figure class="cover">
+                        <img class="figure-img img-fluid" src="<?php echo $core_template; ?>/assets/img/background/top-graphic-contact.jpg" alt="">
+                    </figure>
+                    <div class="container">
+                        <div class="wrapper">
+                            <div class="title typo-lg">ติดต่อเรา</div>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#" class="link">หน้าหลัก</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">ติดต่อเรา</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="default-page contact-page">
+                <div class="container">
+                    <div class="contact-block">
+                        <div class="text-center">
+                            <div class="title">
+                                สถาบันวิจัยและพัฒนาอัญมณีและเครื่องประดับแห่งชาติ (องค์การมหาชน) สำนักงานใหญ่
+                            </div>
+                            <p class="desc">
+                                140, 140/1-3, 140/5 อาคารไอทีเอฟ-ทาวเวอร์ ชั้นที่ 1-4, 6ถนนสีลม แขวงสุริยวงศ์ เขตบางรัก กรุงเทพมหานคร 10500 <br>
+                                เลขประจำตัวผู้เสียภาษีอากร 0 9940 00210 63 9
+                                <br>
+                                <br>
+                                วันและเวลาทำการ : วันจันทร์ - ศุกร์ เวลา 8.30 - 16.30 น.
+                            </p>
+                            <div class="report-link">
+                                <a href="https://www.git.or.th/complainSystem/default.aspx" class="btn btn-primary" title="ระบบร้องเรียนการทุจริตและประพฤติมิชอบ" target="_blank">
+                                    ระบบร้องเรียนการทุจริตและประพฤติมิชอบ
+                                </a>
+                                <a href="https://www.git.or.th/complainSystem/General.aspx" class="btn btn-border-primary" title="ระบบร้องเรียน/ เสนอแนะออนไลน์ (เรื่องทั่วไป)" target="_blank">
+                                    ระบบร้องเรียน/ เสนอแนะออนไลน์ (เรื่องทั่วไป)
+                                </a>
+                            </div>
+                            <hr class="divider">
+                            <div class="contact-list">
+                                <ul class="item-list">
+                                    <li>
+                                        <a href="tel:026344999" class="link" title="Telephone Number">
+                                            <span class="feather icon-phone-call"></span>
+                                            0 2634 4999
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="" class="link" title="Fax Number">
+                                            <span class="feather icon-printer"></span>
+                                            <!-- <span class="lnr lnr-printer"></span> -->
+                                            0 2634 4970
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="mailto:jewelry@git.or.th" class="link" title="Email address">
+                                            <span class="feather icon-mail"></span>
+                                            jewelry@git.or.th
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="map-block">
+                    <div class="iframe-container">
+                        <iframe class="responsive-iframe" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7751.732215638551!2d100.528245!3d13.726555!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2984903b1c6a5de!2sThe%20Gem%20and%20Jewelry%20Institute%20of%20Thailand%20(Public%20Organization)!5e0!3m2!1sth!2sus!4v1664961253439!5m2!1sth!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="form-block">
+                        <div class="title">แบบฟอร์มติดต่อเรา</div>
+                        <hr class="divider">
+                        <form data-toggle="validator" role="form" class="form-default mt-xl-5" method="post">
+                            <div class="row gutters-lg-50 gutters-15">
+                                <div class="col-md-6">
+                                    <div class="form-group has-feedback">
+                                        <label class="control-label" for="contact">ติดต่อ</label>
+                                        <div class="block-control">
+                                            <input type="text" class="form-control" id="contact" placeholder="ร้องเรียน" data-error="">
+                                            <span class="form-control-feedback" aria-hidden="true"></span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group has-feedback">
+                                        <label class="control-label" for="topic">หัวข้อ</label>
+                                        <div class="block-control">
+                                            <input type="text" class="form-control" id="topic" placeholder="หัวข้อ" data-error="" value="ร้องเรียนเกี่ยวกับนโยบายการใช้งานเว็บไซต์">
+                                            <span class="form-control-feedback" aria-hidden="true"></span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group has-feedback">
+                                        <label class="control-label" for="message">ข้อความ</label>
+                                        <div class="block-control">
+                                            <textarea class="form-control form-text-area" rows="10" cols="100" id="message" placeholder="" data-error=""></textarea>
+                                            <span class="form-control-feedback" aria-hidden="true"></span>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-6 order-md-2 order-first">
+                                    <div class="graphic-map">
+                                        <div class="title">แผนที่กราฟฟิค</div>
+                                        <figure class="cover">
+                                            <img src="<?php echo $core_template; ?>/assets/img/static/graphic-map.jpg" alt="แผนที่กราฟฟิค">
+                                        </figure>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row gutters-lg-50 gutters-15">
+                                <div class="col-md-6">
+                                    <div class="form-group has-feedback">
+                                        <label class="control-label" for="fullName">ชื่อ - นามสกุล</label>
+                                        <div class="block-control">
+                                            <input type="text" class="form-control" id="fullName" placeholder="กรอก ชื่อ - นามสกุล" data-error="" value="">
+                                            <span class="form-control-feedback" aria-hidden="true"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group has-feedback">
+                                        <label class="control-label" for="email">อีเมล</label>
+                                        <div class="block-control">
+                                            <input type="email" class="form-control" id="email" placeholder="กรอกอีเมล" data-error="" value="">
+                                            <span class="form-control-feedback" aria-hidden="true"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group has-feedback">
+                                        <label class="control-label" for="address">ที่อยู่</label>
+                                        <div class="block-control">
+                                            <input type="text" class="form-control" id="address" placeholder="กรอกที่อยู่" data-error="" value="">
+                                            <span class="form-control-feedback" aria-hidden="true"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group has-feedback">
+                                        <label class="control-label" for="phoneNumber">เบอร์โทรศัพท์</label>
+                                        <div class="block-control">
+                                            <input type="tel" class="form-control" id="phoneNumber" placeholder="กรอกเบอร์โทรศัพท์" data-error="" value="">
+                                            <span class="form-control-feedback" aria-hidden="true"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row gutters-lg-50 gutters-15 justify-content-center">
+                                <div class="col-md-3 col">
+                                    <button type="submit" id="submitForm" class="btn fluid btn-lg btn-primary" title="ส่งข้อมูล">ส่งข้อมูล</button>
+                                </div>
+                                <div class="col-md-3 col">
+                                    <button type="button" id="cancelForm" class="btn fluid btn-lg btn-border-primary" title="ยกเลิก">ยกเลิก</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="typo-lg text-primary mb-4">เอกสารแนบ</div>
+                    <div class="attachment-slider default-slick">
+                        <?php for ($i = 1; $i <= 5; $i++) { ?>
+                            <div class="item">
+                                <div class="attachment-block">
+                                    <a href="#" class="link" title="เอกสารแนบ">
+                                        <div class="row no-gutters">
+                                            <div class="col-auto">
+                                                <!-- <img class="icon" src="<?php echo $core_template; ?>/assets/img/icon/icon-attachment.svg" alt="attachment icon"> -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="41" viewBox="0 0 32 41">
+                                                    <g data-name="Group 9337" transform="translate(0)">
+                                                        <path data-name="Path 2087" d="M9.75,2h15a1,1,0,0,1,.721.307l11.25,11.7A1,1,0,0,1,37,14.7V38.1A4.832,4.832,0,0,1,32.25,43H9.75A4.832,4.832,0,0,1,5,38.1V6.9A4.832,4.832,0,0,1,9.75,2ZM24.324,4H9.75A2.831,2.831,0,0,0,7,6.9V38.1A2.831,2.831,0,0,0,9.75,41h22.5A2.831,2.831,0,0,0,35,38.1v-23Z" transform="translate(-5 -2)" fill="#013f94" />
+                                                        <path data-name="Path 2088" d="M32.438,15.438H21a1,1,0,0,1-1-1V3a1,1,0,0,1,2,0V13.438H32.438a1,1,0,0,1,0,2Z" transform="translate(-1.438 -2)" fill="#013f94" />
+                                                        <path data-name="Path 2089" d="M26.75,20.5H12a1,1,0,0,1,0-2H26.75a1,1,0,0,1,0,2Z" transform="translate(-3.375 2.949)" fill="#013f94" />
+                                                        <path data-name="Path 2090" d="M26.75,26.5H12a1,1,0,0,1,0-2H26.75a1,1,0,0,1,0,2Z" transform="translate(-3.375 4.75)" fill="#013f94" />
+                                                        <path data-name="Path 2091" d="M15.813,14.5H12a1,1,0,0,1,0-2h3.813a1,1,0,0,1,0,2Z" transform="translate(-3.518 1.15)" fill="#013f94" />
+                                                    </g>
+                                                </svg>
+                                            </div>
+                                            <div class="col">
+                                                <div class="title typo-sm">มาตรการป้องกันการรับสินบน (ไฟล์ PDF ขนาด 293 KB)</div>
+                                                <div class="subtitle typo-x">Type file : 009 Mb | File size: .pdf</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <?php include('inc/footer.php'); ?>
+    </div>
+
+    <?php include('inc/loadscript.php'); ?>
+
+</body>
+
+</html>
---
+++ _html/member.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('inc/metatag.php'); ?>
+    <?php include('inc/loadstyle.php'); ?>
+</head>
+
+<body>
+
+    <div class="global-container">
+        <?php include('inc/header.php'); ?>
+
+        <section class="site-container">
+
+            <div class="default-header">
+                <div class="top-graphic mb-4 text-primary">
+                    <figure class="cover">
+                        <img class="figure-img img-fluid" src="<?php echo $core_template; ?>/assets/img/background/top-graphic-contact.jpg" alt="">
+                    </figure>
+                    <div class="container">
+                        <div class="wrapper">
+                            <div class="title typo-lg">สมาชิกสัมพันธ์</div>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#" class="link">หน้าหลัก</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">สมาชิกสัมพันธ์</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="default-page member-page">
+                <div class="container">
+                    <div class="editor-content">
+                        <div class="h-title">
+                            ประเภทสมาชิกและสิทธิประโยชน์
+                        </div>
+                        <div class="member-block">
+                            <div class="title">ประเภทนิติบุคคล</div>
+                            <hr class="divider">
+                            <div class="subtitle">ประเภทสมาชิกและสิทธิประโยชน์สิทธิพิเศษที่จะได้รับมีดังนี้</div>
+                            <ol class="item-list checked">
+                                <li>
+                                    ส่วนลด 10% จากราคาค่าบริการตรวจสอบอัญมณีและบริการตรวจสอบโลหะมีค่า
+                                </li>
+                                <li>
+                                    ส่วนลด 10% สำหรับบริการตรวจสอบแบบเร่งด่วน Express และ Fast track
+                                </li>
+                                <li>
+                                    ส่วนลด 5% เมื่อซื้อ 90 ใบ จาก 126,000 บาท เหลือ 119,700 บาท
+                                </li>
+                                <li>
+                                    ส่วนลด 10% ต่อคน จากราคาค่าฝึกอบรมปกติ สำหรับหลักสูตร ระยะยาว (เฉพาะหลักสูตรภาษาไทย)
+                                </li>
+                                <li>
+                                    ส่วนลด 15% เมื่อสมัครพร้อมกันตั้งแต่ 2 หลักสูตร หรือ 2 คนขึ้นไป จากราคาค่าฝึกอบรมปกติ สำหรับหลักสูตรระยะยาว (เฉพาะหลักสูตร ภาษาไทย)
+                                </li>
+                                <li>
+                                    ส่วนลด 10% จากราคาค่าบริการผลิตชิ้นงานต้นแบบเครื่องประดับ
+                                </li>
+                                <li>
+                                    ส่วนลด 10% เมื่อซื้อของที่ระลึกจากพิพิธภัณฑ์ ณ เคาน์เตอร์จำหน่าย
+                                </li>
+                                <li>
+                                    ฟรีค่าเข้าห้องสมุด และค่าเข้าชมพิพิธภัณฑ์ พร้อมผู้ติดตาม 2 ท่าน
+                                </li>
+                                <li>
+                                    ส่วนลด 15% จากราคาหนังสือหรือเอกสารปกติ
+                                </li>
+                                <li>
+                                    ได้รับสิทธิพิเศษในการเข้าร่วมกิจกรรมต่างๆ
+                                </li>
+                                <li>
+                                    สงวนสิทธิ์การจัดโปรโมชั่นพิเศษใดๆ ให้สิทธิพิเศษกับสมาชิกก่อน
+                                </li>
+                                <li>
+                                    บริการให้คำปรึกษาด้านอัญมณีโดยนักอัญมณีศาสตร์ (ในเวลาทำการ)
+                                </li>
+                            </ol>
+                            <div class="subtitle">หมายเหตุ</div>
+                            <ol class="item-list note">
+                                <li>
+                                    > สิทธิประโยชน์ของสมาชิกดังกล่าวข้างต้นนี้ สามารถใช้ได้ถึงวันที่ 31 ธันวาคม 2565
+                                </li>
+                                <li>
+                                    > สถาบันขอสงวนสิทธิ์ในการเปลี่ยนแปลงสิทธิประโยชน์ของสมาชิกโดยไม่ได้แจ้ง ให้ทราบล่วงหน้า
+                                </li>
+                            </ol>
+                        </div>
+                        <div class="member-block">
+                            <div class="title">ประเภทบุคคลธรรมดา</div>
+                            <hr class="divider">
+                            <div class="subtitle">ประเภทสมาชิกและสิทธิประโยชน์สิทธิพิเศษที่จะได้รับมีดังนี้</div>
+                            <ol class="item-list checked">
+                                <li>
+                                    ส่วนลด 10% จากราคาค่าบริการตรวจสอบอัญมณีและบริการ ตรวจสอบโลหะมีค่า
+                                </li>
+                                <li>
+                                    ส่วนลด 10% สำหรับบริการตรวจสอบแบบเร่งด่วน Express และ Fast track
+                                </li>
+                                <li>
+                                    ส่วนลด 5% เมื่อซื้อ 90 ใบ จาก 126,000 บาท เหลือ 119,700 บาท
+                                </li>
+                                <li>
+                                    ส่วนลด 10% ต่อคน จากราคาค่าฝึกอบรมปกติ สำหรับหลักสูตร ระยะยาว (เฉพาะหลักสูตรภาษาไทย)
+                                </li>
+                                <li>
+                                    ส่วนลด 15% เมื่อสมัครพร้อมกันตั้งแต่ 2 หลักสูตร หรือ 2 คนขึ้นไป จากราคาค่าฝึกอบรมปกติ สำหรับหลักสูตรระยะยาว (เฉพาะหลักสูตร ภาษาไทย)
+                                </li>
+                                <li>
+                                    ส่วนลด 10% จากราคาค่าบริการผลิตชิ้นงานต้นแบบเครื่องประดับ
+                                </li>
+                                <li>
+                                    ส่วนลด 10% เมื่อซื้อของที่ระลึกจากพิพิธภัณฑ์ ณ เคาน์เตอร์จำหน่าย
+                                </li>
+                                <li>
+                                    ฟรีค่าเข้าห้องสมุด และค่าเข้าชมพิพิธภัณฑ์
+                                </li>
+                                <li>
+                                    ส่วนลด 15% จากราคาหนังสือหรือเอกสารปกติ
+                                </li>
+                                <li>
+                                    ได้รับสิทธิพิเศษในการเข้าร่วมกิจกรรมต่างๆ
+                                </li>
+                                <li>
+                                    สงวนสิทธิ์การจัดโปรโมชั่นพิเศษใดๆ ให้สิทธิพิเศษกับสมาชิกก่อน
+                                </li>
+                                <li>
+                                    บริการให้คำปรึกษาด้านอัญมณีโดยนักอัญมณีศาสตร์ (ในเวลาทำการ)
+                                </li>
+                            </ol>
+                            <div class="subtitle">หมายเหตุ</div>
+                            <ol class="item-list note">
+                                <li>
+                                    > สิทธิประโยชน์ของสมาชิกดังกล่าวข้างต้นนี้สามารถใช้ได้ถึงวันที่ 31 ธันวาคม 2565
+                                </li>
+                                <li>
+                                    > สถาบันขอสงวนสิทธิในการเปลี่ยนแปลงสิทธิประโยชน์ของสมาชิกโดยไม่ได้แจ้งให้ทราบล่วงหน้า
+                                </li>
+                            </ol>
+                        </div>
+                        <div class="member-block">
+                            <div class="title">สมาชิกโครงการซื้อด้วยความมั่นใจ (Buy With Confidence : BWC)</div>
+                            <hr class="divider">
+                            <a href="https://bwc.git.or.th/Register/Detail" class="btn btn-lg btn-primary" target="_blank">
+                                สิทธิประโยชน์ของสมาชิกที่จะได้รับมีดังนี้
+                                <svg xmlns="http://www.w3.org/2000/svg" width="51.235" height="7.199" viewBox="0 0 51.235 7.199">
+                                    <path data-name="Path 5" d="M4670.6,5544.179h50.033l-6.306-6.347" transform="translate(-4670.602 -5537.48)" fill="none" stroke="#fff" stroke-width="1" />
+                                </svg>
+                            </a>
+                        </div>
+                        <div class="divider"></div>
+                        <div class="youtube-block">
+                            <div class="iframe-container">
+                                <iframe class="responsive-iframe" src="https://www.youtube.com/embed/4r9TbKLCus0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="typo-lg text-primary mb-4">เอกสารแนบ</div>
+                    <div class="attachment-slider default-slick">
+                        <?php for ($i = 1; $i <= 5; $i++) { ?>
+                            <div class="item">
+                                <div class="attachment-block">
+                                    <a href="#" class="link" title="เอกสารแนบ">
+                                        <div class="row no-gutters">
+                                            <div class="col-auto">
+                                                <!-- <img class="icon" src="<?php echo $core_template; ?>/assets/img/icon/icon-attachment.svg" alt="attachment icon"> -->
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="41" viewBox="0 0 32 41">
+                                                    <g data-name="Group 9337" transform="translate(0)">
+                                                        <path data-name="Path 2087" d="M9.75,2h15a1,1,0,0,1,.721.307l11.25,11.7A1,1,0,0,1,37,14.7V38.1A4.832,4.832,0,0,1,32.25,43H9.75A4.832,4.832,0,0,1,5,38.1V6.9A4.832,4.832,0,0,1,9.75,2ZM24.324,4H9.75A2.831,2.831,0,0,0,7,6.9V38.1A2.831,2.831,0,0,0,9.75,41h22.5A2.831,2.831,0,0,0,35,38.1v-23Z" transform="translate(-5 -2)" fill="#013f94" />
+                                                        <path data-name="Path 2088" d="M32.438,15.438H21a1,1,0,0,1-1-1V3a1,1,0,0,1,2,0V13.438H32.438a1,1,0,0,1,0,2Z" transform="translate(-1.438 -2)" fill="#013f94" />
+                                                        <path data-name="Path 2089" d="M26.75,20.5H12a1,1,0,0,1,0-2H26.75a1,1,0,0,1,0,2Z" transform="translate(-3.375 2.949)" fill="#013f94" />
+                                                        <path data-name="Path 2090" d="M26.75,26.5H12a1,1,0,0,1,0-2H26.75a1,1,0,0,1,0,2Z" transform="translate(-3.375 4.75)" fill="#013f94" />
+                                                        <path data-name="Path 2091" d="M15.813,14.5H12a1,1,0,0,1,0-2h3.813a1,1,0,0,1,0,2Z" transform="translate(-3.518 1.15)" fill="#013f94" />
+                                                    </g>
+                                                </svg>
+                                            </div>
+                                            <div class="col">
+                                                <div class="title typo-sm">มาตรการป้องกันการรับสินบน (ไฟล์ PDF ขนาด 293 KB)</div>
+                                                <div class="subtitle typo-x">Type file : 009 Mb | File size: .pdf</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                    <div class="row pt-5 text-right">
+                        <div class="col-12">
+                            <a href="" class="btn btn-border-primary" title="btn btn-primary">กลับ</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <?php include('inc/footer.php'); ?>
+    </div>
+
+    <?php include('inc/loadscript.php'); ?>
+
+</body>
+
+</html>
