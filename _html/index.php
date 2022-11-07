0c98425041181f5f2c8c299a3c085efc027c01af
Ohm <jeraphan2000@gmail.com>
2022-11-07 11:36:58 +0700

    menu li fluid

--- _html/index.php
+++ _html/index.php
@@ -14,8 +14,31 @@
         <section class="site-container">
             <div class="main-page">
 
-                <div class="container -xl">
-                    <div class="video-gallery">
+                <div class="banner-I-block">
+                    <div class="container">
+                        <div class="banner-txt text-center">
+                            <div class="title text-uppercase">
+                                GIT <span class="text-secondary">Carat</span> Application
+                            </div>
+                            <div class="subtitle">Platform ให้คำปรึกษา ทุกเรื่องอัญมณีและเครื่องประดับครั้งแรกบนมือถือวันนี้</div>
+                            <div class="desc">ดาวน์โหลดฟรีได้แล้วทั้ง IOS และ Android</div>
+                            <div class="action">
+                                <a href="https://apps.apple.com/th/app/carat/id1498901876?itsct=apps_box_badge&amp;itscg=30200" title="Download on the App Store" target="_blank">
+                                    <img src="<?php echo $core_template; ?>/assets/img/icon/appstore.svg" alt="Download on the App Store">
+                                </a>
+                                <a href="https://play.google.com/store/apps/details?id=com.ultimate.carat&hl=th" title="Get it on Google Play" target="_blank">
+                                    <img src="<?php echo $core_template; ?>/assets/img/icon/playstore.svg" alt="Get it on Google Play">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="graphic">
+                            <img src="<?php echo $core_template; ?>/assets/img/static/graphic-smartphone.png" alt="smartphone">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="video-gallery">
+                    <div class="container -xl">
                         <div class="h-title text-center">GIT VDO</div>
                         <ul class="nav nav-pills default-nav-tab">
                             <li>
