6aa766d63e84ef1a339f5bba17770a20c11be3ca
Phoo <phoo319@gmail.com>
2022-11-09 12:50:44 +0700

    edit config

--- front/controller/script/_mainpage/config.php
+++ front/controller/script/_mainpage/config.php
@@ -72,6 +72,8 @@ $config['ab_nm']['main']['masterkey'] = "ab_nm";
 $config['km_t3']['main']['masterkey'] = "km_t3";
 $config['setting']['main']['masterkey'] = "setting";
 $config['lcf_s']['main']['masterkey'] = "lcf_s";
+$config['video']['vdo']['masterkey'] = "vdo";
+$config['photo']['ptg']['masterkey'] = "ptg";
 
 $config['req']['main']['masterkey'] = "req";
 //PDPA
