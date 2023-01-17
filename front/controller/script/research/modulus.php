26009cba6c1dbb5f622063a95371a1a18bd640ab
northpanupong <panupong.w@wewebplus.com>
2023-01-17 21:41:38 +0700

    fix research modulus

--- front/controller/script/research/modulus.php
+++ front/controller/script/research/modulus.php
@@ -250,9 +250,9 @@ class researchPage
       $sql .= " AND " . $config['cmsg']['db']['main'] . "." . $config['cmsg']['db']['main'] . "_gid = '" . $gid . "' ";
     }
 
-    if (!empty($year)) {
-      $sql .= " AND YEAR(" . $config['cms']['db']['main'] . "." . $config['cms']['db']['main'] . "_credate) = '" . $year . "' ";
-    }
+    // if (!empty($year)) {
+    //   $sql .= " AND YEAR(" . $config['cms']['db']['main'] . "." . $config['cms']['db']['main'] . "_credate) = '" . $year . "' ";
+    // }
 
     $sql .= " GROUP BY " . $config['cmsg']['db']['main'] . "." . $config['cmsg']['db']['main'] . "_id ORDER  BY " . $config['cmsg']['db']['main'] . "." . $config['cmsg']['db']['main'] . "_order ".$order." ";
 
@@ -319,9 +319,9 @@ class researchPage
       $sql .= " AND " . $config['cms']['db']['main'] . "." . $config['cms']['db']['main'] . "_id = '" . $id . "' ";
     }
 
-    if (!empty($year)) {
-      $sql .= " AND YEAR(" . $config['cms']['db']['main'] . "." . $config['cms']['db']['main'] . "_credate) = '" . $year . "' ";
-    }
+    // if (!empty($year)) {
+    //   $sql .= " AND YEAR(" . $config['cms']['db']['main'] . "." . $config['cms']['db']['main'] . "_credate) = '" . $year . "' ";
+    // }
 
     if (!empty($sid)) {
       $sql .= " AND " . $config['cms']['db']['main'] . "." . $config['cms']['db']['main'] . "_sid = '" . $sid . "' ";
